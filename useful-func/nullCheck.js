eea2908e61c92e90c7b5450590c6be529b2000ff
vboyoung <vboyoungh@gamil.com>
2021-07-01 15:47:11 +0900

    short circuit

---
+++ useful-func/nullCheck.js
@@ -0,0 +1,73 @@
+
+
+// TypeError: Cannot read property 'name' of null 
+
+function print(person) {
+    if (person === undefined) {
+      console.log('person이 없네요');
+      return;
+    }
+    console.log(person.name);
+  }
+  
+const person = null;
+print(person);
+
+
+
+
+/**
+ *  javascript에서 undefined, null은 Falsy 한 값이다.
+ *  Falsy 한 값 앞에 느낌표를 붙여주면 Truthy로 전환된다.
+ *  이는 즉 undifined, null일 경우 동작하게 조건문을 동작시킨다.
+ *  
+ *  앞으로 null , undifined  check는 아래와 같이 하기
+ */
+
+
+console.log(!undefined)  // undefined true
+console.log(!null)  //null true
+console.log(!0)
+console.log(!'')
+console.log(!NaN)
+
+
+function print(person) {
+
+    //null, undifined 예외처리 
+    if (!person) {
+      console.log('person이 없네요');
+      return;
+    }
+    console.log(person.name);
+  }
+  
+  const person = null;
+  print(person);
+
+
+
+
+
+
+/**
+ * 삼항 연산자를 이용
+ * 
+ */
+
+const value = { a: 1 };
+const truthy = !!value
+
+if (value) {
+  console.log('value 가 Truthy 하네요.');
+}
+
+
+
+const value = { a: 1 };
+const truthy = value ? true : false
+
+if (value) {
+  console.log('value 가 Truthy 하네요.');
+}
+
---
+++ useful-func/short-circuit.js
@@ -0,0 +1,82 @@
+
+true && true  // true
+true && false // false
+
+true  || false // true
+false || true  // true
+
+
+
+// null은 falshy한 값
+
+console.log(null && '여기 안본다') // null 
+console.log( 1 && '여기 본다')   // and 연산자에서는 앞의 값이 truthy하면 앞은 안나오고 뒤에 연산자가 나온다. 
+
+console.log(null || '여기 본다')  // 여기 본다
+console.log( 1 || '여기 안본다') // or 연산자에서는 앞에 값이 truthy하면 뒤에 연산자가 나오지 않는다. true 이고 끝
+
+
+/**
+ *  결론
+ *  && : true 이면 다음 true,   false 이면 끝
+ *  || : true 이고 끝,  false 이고 다음 false  (예외처리일때 사용)
+ */
+
+
+/* 기존 코드 */
+const dog = {
+    name: '멍멍이'
+}
+
+function getName(animal) {
+
+    if(animal) {
+        return animal.name
+    }
+
+    return undefined
+}
+
+const name01 = getName()
+console.log(name01);
+
+
+
+
+
+/*  && 연산자로 코드를 단축시켜보자  */ 
+
+const namelessDog = {name: ''}
+
+function getNameani (animal) {
+
+   const name = animal && animal.name
+    
+    if(!name) { //name이 falsy 한 값이면 truthy한 값으로 변경 후 예외처리
+        return '이름이 없는 동물입니다'
+    }
+    return name //thruthy 한 값이면 return 
+}
+
+const name = getNameani(namelessDog)
+console.log(name)
+
+
+
+
+/*  || 연산자로 코드를 더해서 더 단축시켜보자  */ 
+
+const namelessDogPlus = {name: ''}
+
+function getNameaniPlus (animal) {
+
+    const name = animal && animal.name
+    return name || '이름이 없는 동물입니다.'
+
+}
+
+const namePlus = getNameaniPlus(namelessDogPlus)
+console.log(namePlus)
+
+
+
